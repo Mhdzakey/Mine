f79daff73ff15b41fa4cc5c3793065a049ade030
Zakey <mhdzakaria360@gmail.com>
2024-03-15 00:32:56 +0500

    react class 3

---
+++ 08_React/class_3/02_tourism_project/src/components/Tours.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tours = () => {
+  return (
+    <div>
+        <div>
+            <h2>Plan With Xakey</h2>
+        </div> 
+               
+    </div>
+  )
+}
+
+export default Tours
