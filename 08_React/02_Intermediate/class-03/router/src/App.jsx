7c36c0a37a6f8e58917a390ecfa41414ed15e627
Zakey <mhdzakaria360@gmail.com>
2024-03-18 04:42:27 +0500

    implement upto login forma page

--- 08_React/02_Intermediate/class-03/router/src/App.jsx
+++ 08_React/02_Intermediate/class-03/router/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Dashbord from './pages/Dashbord'
 const App = () => {
+  const [isLoggedIn,setIsLoggedIn] = useState(true);
   return (
-    <div className='bg-slate-500 flex text-white items-center justify-center'>App</div>
+    
+    <div>
+      <Navbar isLoggedIn = {isLoggedIn} setIsLoggedIn ={setIsLoggedIn}/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashbord/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ 08_React/02_Intermediate/class-03/router/src/pages/Dashbord.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dashbord = () => {
+  return (
+    <div className='flex justify-center items-center'
+    >Welcomne to Study Notion</div>
+  )
+}
+
+export default Dashbord
